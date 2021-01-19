534884223e43ae9253aa109e288932e21c5393b1
shenqz <9509394@qq.com>
2021-01-19 20:12:42 +0800

    config code formater

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+/*
+ * @Author: shen
+ * @Date: 2021-01-19 19:33:43
+ * @LastEditors: shen
+ * @LastEditTime: 2021-01-19 20:09:48
+ * @Description: entry file
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
 createApp(App)
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
-import { createStore } from "vuex";
+/*
+ * @Author: shen
+ * @Date: 2021-01-19 19:33:43
+ * @LastEditors: shen
+ * @LastEditTime: 2021-01-19 20:11:21
+ * @Description:
+ */
+import { createStore } from 'vuex'
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
-});
+  modules: {},
+})
